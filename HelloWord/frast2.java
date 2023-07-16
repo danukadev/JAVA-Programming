c1919c00c5480f1205ca2d2b90a0fd094d2db6e2
DANUKA SANDAKELUM <send2sadankelum@gmail.com>
2023-07-16 22:43:20 +0530

    create variable

---
+++ HelloWord/frast2.java
@@ -0,0 +1,18 @@
+public class frast2 {
+    public static void main(String args[])
+    {
+
+        int number =10;
+        System.out.print(number+" ");
+        System.out.println("number");
+
+
+
+
+
+    }
+
+    
+
+    
+}
