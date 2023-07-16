972d404ce0448c73c0ac53f6549de4ff74a56ef0
DANUKA SANDAKELUM <send2sadankelum@gmail.com>
2023-07-17 00:05:45 +0530

    User Input

---
+++ input/Input.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Input {
+     public static void main(String args[]) {
+        Scanner dan = new Scanner (System.in);
+
+        System.out.println(dan.next());
+        System.out.println(dan.nextLine());
+
+        
+    }
+    
+}
