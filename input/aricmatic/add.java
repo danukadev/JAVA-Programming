0ec2f2f67d35fad4cceaa662fe72828649564931
DANUKA SANDAKELUM <send2sadankelum@gmail.com>
2023-07-17 19:46:45 +0530

    ssgreh

---
+++ input/aricmatic/add.java
@@ -0,0 +1,18 @@
+
+
+public class add {
+     public static void main( String[]args)
+      {
+
+        int num1;
+        int num2;
+        int sum;
+        num1=10;
+        num2=427;
+        sum=num1 + num2;
+
+        System.out.println(sum);
+        
+    }
+    
+}
