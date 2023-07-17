1d09e02535328cf93670af9d210a8b3630ca0fee
DANUKA SANDAKELUM <send2sadankelum@gmail.com>
2023-07-17 20:00:09 +0530

    Multipication

---
+++ input/aricmatic/multiple/multiple.java
@@ -0,0 +1,16 @@
+
+
+public class multiple {
+    public static void main(String []args) {
+        int Num1;
+        int Num2;
+        Num1=10;
+        Num2=20;
+        int Multiple;
+        Multiple=Num1*Num2;
+    
+        System.out.println(Multiple);
+        
+    }
+    
+}
